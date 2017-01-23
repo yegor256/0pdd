248f2f9419dcfdcf2e33e90adbfa762b93a6a9f3
Yegor Bugayenko <yegor256@gmail.com>
2017-01-23 14:25:12 +0200

    #46 deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,12 @@
 set -e
 
 cd $(dirname $0)
+bundle update
+sed -i -s 's|Gemfile.lock||g' .gitignore
 cp /code/home/assets/0pdd/config.yml .
 git add config.yml
+git add Gemfile.lock
+git add .gitignore
 git commit -m 'config.yml for heroku'
 trap 'git reset HEAD~1 && rm config.yml' EXIT
 git push heroku master -f
