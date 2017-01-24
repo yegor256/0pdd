ca9e2a7fa1cb4a04d1ef83a882384280cf022317
Vivek Poddar <vivekimsit@gmail.com>
2017-01-24 11:20:05 +0530

    Fixes: can't modify frozen String.

--- objects/github_tickets.rb
+++ objects/github_tickets.rb
@@ -100,12 +100,13 @@ source code, that's why I closed this issue." +
   end
 
   def truncate(text, limit = 40, separator = '...')
-    unless limit >= text.length
+    if limit < text.length
       length_with_room_for_omission = limit - separator.length
       stop = text.rindex(' ', length_with_room_for_omission) || 0
-      text = "#{text[0...stop]}#{separator}"
+      "#{text[0...stop]}#{separator}"
+    else
+      text
     end
-    text
   end
 
   def client
