aef84b92df7caeb30aa1213e66c4b6afb1b8e4c7
yegor256 <yegor256@gmail.com>
2017-12-04 16:46:35 +0200

    typo

--- objects/job_commiterrors.rb
+++ objects/job_commiterrors.rb
@@ -40,7 +40,7 @@ class JobCommitErrors
       "I wasn't able to retrieve PDD puzzles from the code base and \
 submit them to GitHub. If you \
 think that it's a bug on our side, please submit it to \
-[yegor256/0pdd](https://github.com/yegor256/0pdd/issues)):\n\n\
+[yegor256/0pdd](https://github.com/yegor256/0pdd/issues):\n\n\
 > #{Truncated.new(e.message.gsub(/\s/, ' '), 300)}\n\n
 Please, copy and paste this stack trace to GitHub:\n\n
 ```\n#{e.class.name}\n#{e.message}\n#{e.backtrace.join("\n")}\n```"
