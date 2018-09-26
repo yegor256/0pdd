dc7052675e6e59f1885f05788afc5dfb0579f6ab
snugghash <snugghash@gmail.com>
2018-09-26 13:02:11 -0400

    Add test for closing #234

---
+++ test/test_diff_complicated.rb
@@ -0,0 +1,55 @@
+require 'nokogiri'
+require 'ostruct'
+require 'test/unit'
+require_relative '../objects/diff'
+
+# Complicated diff test.
+class TestDiff < Test::Unit::TestCase
+  def test_notification_on_parent_solved_with_others_unsolved
+    tickets = Tickets.new
+    before = Nokogiri::XML(
+      '<puzzles>
+        <puzzle alive="true">
+          <id>100-1</id>
+          <issue>100</issue>
+          <ticket>999</ticket>
+        </puzzle>
+        <puzzle alive="true">
+          <id>100-2</id>
+          <issue>101</issue>
+          <ticket>999</ticket>
+          <children>
+            <puzzle alive="true">
+              <id>101-1</id>
+              <issue>13</issue>
+              <ticket>101</ticket>
+            </puzzle>
+          </children>
+        </puzzle>
+      </puzzles>'
+    )
+    after = Nokogiri::XML(before.to_s)
+    after.xpath('//puzzle[id="100-2"]')[0]['alive'] = 'false'
+    Diff.new(before, after).notify(tickets)
+    assert(
+      tickets.messages.length == 1,
+      "Wrong about of msgs (#{tickets.messages.length}): #{tickets.messages}"
+    )
+    assert(
+      tickets.messages[0] ==
+      '999 2 puzzles [#100](), [#13]() are still not solved; solved: [#101]().',
+      "Text is wrong: #{tickets.messages[0]}"
+    )
+  end
+
+  class Tickets
+    attr_reader :messages
+    def initialize
+      @messages = []
+    end
+
+    def notify(ticket, text)
+      @messages << "#{ticket} #{text}"
+    end
+  end
+end
