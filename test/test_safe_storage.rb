af79f5b53bcf7bc010b3deaef42d191186f17533
yegor256 <yegor256@gmail.com>
2019-02-05 15:57:50 +0300

    #263 fixed

--- test/test_safe_storage.rb
+++ test/test_safe_storage.rb
@@ -34,7 +34,25 @@ class TestSafeStorage < Test::Unit::TestCase
     storage = SafeStorage.new(FakeStorage.new)
     storage.save(
       Nokogiri::XML(
-        '<puzzles date="2017-01-01T01:01:01Z" version="1.0"/>'
+        '<?xml version="1.0"?>
+        <puzzles xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:noNamespaceSchemaLocation="http://www.0pdd.com/puzzles.xsd"
+          date="2016-12-08T12:00:49Z" version="0.0.0">
+          <puzzle alive="true">
+            <issue>unknown</issue>
+            <ticket>1</ticket>
+            <estimate>10</estimate>
+            <role>DEV</role>
+            <id>1-5e0e29d8</id>
+            <lines>7-7</lines>
+            <body>create mvvm model (see main page) for this page</body>
+            <file>attendance/lib/login_page.dart</file>
+            <author>@ammaratef45</author>
+            <email>a_atef_test@gmail-test.com</email>
+            <time>2019-01-16T17:08:45Z</time>
+            <children/>
+          </puzzle>
+        </puzzles>'
       )
     )
   end
