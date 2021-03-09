473dcc21cbeb19ccc546bf96169116e69a782985
yegor256 <yegor256@gmail.com>
2021-03-09 08:05:31 +0300

    #299 year up

--- test/test_logged_tickets.rb
+++ test/test_logged_tickets.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2020 Yegor Bugayenko
+# Copyright (c) 2016-2021 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -28,7 +28,7 @@ require_relative '../objects/logged_tickets'
 
 # LoggedTickets test.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2016-2020 Yegor Bugayenko
+# Copyright:: Copyright (c) 2016-2021 Yegor Bugayenko
 # License:: MIT
 class TestLoggedTickets < Test::Unit::TestCase
   def test_submits_tickets
