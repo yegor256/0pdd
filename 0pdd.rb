ff84a0889f2c69734983d253b163914de3ac5df4
Ayomide Bakare <bakkareayomideo@gmail.com>
2022-05-24 12:10:59 +0300

    [#312] use constructor in github client

--- 0pdd.rb
+++ 0pdd.rb
@@ -104,7 +104,7 @@ configure do
     end
   end
   set :server_settings, timeout: 25
-  set :github, GithubClient.new(config)
+  set :github, Github.new(config).client
   set :dynamo, Dynamo.new(config).aws
   set :glogin, GLogin::Auth.new(
     config['github']['client_id'],
