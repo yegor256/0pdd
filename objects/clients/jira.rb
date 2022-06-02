0001ca8d2c963766d5d2d38383436b85961d816f
Arina Cheverda <amoriodi@yandex-team.ru>
2022-06-02 12:46:46 +0200

    jira client class basic implementation

--- objects/clients/jira.rb
+++ objects/clients/jira.rb
@@ -17,3 +17,38 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+require 'rubygems'
+require 'jira-ruby'
+
+#
+# Jira client
+# API: https://github.com/sumoheavy/jira-ruby
+#
+class JiraClient
+  def initialize(config = {})
+    @config = config
+  end
+
+  def client
+    client = if @config['testing']
+               require_relative '../../test/fake_jira'
+               FakeJira.new
+             else
+               username = @config['jira']['username'] if @config['jira']
+               token = @config['jira']['token'] if @config['jira']
+               options = {
+                 username: username,
+                 password: token,
+                 site: 'http://localhost:8080/', # or 'https://<your_subdomain>.atlassian.net/' # often blank
+                 auth_type: :basic,
+                 read_timeout: 120
+               }
+               JIRA::Client.new(options)
+             end
+    TracePoint.new(:call) do |tp|
+      puts "#{tp.defined_class}##{tp.method_id}()" if tp.defined_class == client.class
+    end.enable
+    client
+  end
+end
