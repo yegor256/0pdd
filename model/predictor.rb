18dfe6e6bf3057a546f43269dbc3dc831f4b7fbd
Ayomide Bakare <bakkareayomideo@gmail.com>
2022-06-06 13:35:10 +0300

    [#318] fix bug in predictor class

--- model/predictor.rb
+++ model/predictor.rb
@@ -37,32 +37,29 @@ class Predictor
     end
   end
 
-  def f(model_weights, **options)
+  def f(weights, **options)
     data = options[:data]
     true_order = options[:true_order]
     kns = []
-    # history_per_backlog = []
     (0...data.size).each do |i|
       x = data[i]
       y = true_order[i].first(x.size)
-      m = model_weights.first(x.size)
-      preds = predict(m, x, y)
+      preds = predict(weights, x, y)
       kn = normalised_kendall_tau_distance(preds, y)
       kns.append(kn)
     end
     kns.sum / kns.size # mean
   end
 
-  def train(model_weights, data, true_order, debug = false)
-    ranks = predict(model_weights, data, true_order, debug)
+  def train(weights, data, true_order, debug = false)
+    ranks = predict(weights, data, true_order, debug)
     normalised_kendall_tau_distance(ranks, true_order)
   end
 
-  def predict(model_weights, data, true_order = [], debug = false)
+  def predict(weights, data, true_order = [], debug = false)
     ranks = []
     (0...data.size).each do |i|
       row = data[i]
-      weights = model_weights.first(row.size)
       r = forward_one(weights, row)
       ranks.append(r)
     end
@@ -73,30 +70,15 @@ class Predictor
     ranks
   end
 
-  def forward_one(model_weights, data)
+  def forward_one(weights, data)
     x = data.clone.map(&:clone).flatten
-    w = model_weights
+    w = weights.first(x.size)
     x = Vector[*x].dot(Vector[*w])
     w.map { |c| x += c }[0]
   end
 
-  def kendall(model_weights, data, true_order, _full = true)
-    x = predict(model_weights, data)
+  def kendall(weights, data, true_order, _full = true)
+    x = predict(weights, data)
     normalised_kendall_tau_distance(x, true_order)
   end
 end
-
-# optimizer = ps.single.GlobalBestPSO(n_particles=400, dimensions=param_size, options=options)
-# cost, pos = optimizer.optimize(p.train_full, iters=15,
-#                   data=X,
-#                   true_order=Y,
-#                   verbose=True)
-# #  where X is the set of repositories and Y is the set of order of task solving, like [[1,2,3], [5,4,3,2,1]]
-
-# for repo in repositories
-#   predictions = p.forward(pos, x, y)
-#   kn = normalised_kendall_tau_distance(predictions, y)
-# # here x - a repo, and y - a order of task execution
-
-# y = [[1, 2, 3]]
-# data = [[[0.1, 0.3, 0.8, 1, -0.2, -0.6], [1, -0.3, 0.5, 0, 0.2, -0.6], [0.3, 0.3, -0.9, -0.5, -1, 0.7]]]
