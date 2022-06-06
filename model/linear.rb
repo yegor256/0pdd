6104ef4df071b3ae1c87cf5e92f7ed5ff4a3c6b8
Ayomide Bakare <bakkareayomideo@gmail.com>
2022-06-06 13:42:34 +0300

    [#318] refactor code in linear model

--- model/linear.rb
+++ model/linear.rb
@@ -32,18 +32,20 @@ class LinearModel
     else
       # get x and y data for puzzles
       samples, _labels = extract_features(puzzles)
-      # x = [[0.1, 0.3, 0.8, 1, -0.2, -0.6], [1, -0.3, 0.5, 0, 0.2, -0.6], [0.3, 0.3, -0.9, -0.5, -1, 0.7]]
-      # y = [1, 3, 2]
-      ranks = clf.predict(weights, samples, true) # model rank of puzzles if weights are loaded
+      ranks = clf.predict(weights, samples[0], true) # model rank of puzzles if weights are loaded
     end
     ranks.map(&:to_i)
   end
 
   private
 
+  def replace_nil(arr, with = 0)
+    arr.map { |x| x.nil? ? with : x }
+  end
+
   def get_features_labels(samples)
     x = samples.map do |_, s|
-      [
+      replace_nil([
         s['time_estimate'],
         s['n_characters'],
         s['level'],
@@ -53,7 +55,7 @@ class LinearModel
         s['time_after'],
         s['n_additions'],
         s['n_deletions']
-      ].append(s['vectorized_description'])
+      ].append(s['vectorized_description']))
     end
     y = samples.map { |_, s| s['closed'] ? Time.parse(s['closed']).to_i : 0 }.map.with_index.sort.map(&:last)
     [[x], [y]] # single backlog of puzzles
@@ -90,26 +92,6 @@ class LinearModel
   end
 
   def train(clf)
-    # todo
-    # 1. load xml tree for repo from storage.
-    # 2. *if xml tree is empty, create new xml tree using _puzzles.
-    # 3. *if xml tree is not empty, add _puzzles to xml tree.
-    # 4. process puzzles in xml tree.
-    # # Features (x)
-    # #  1. get estimate for each puzzle
-    # #  2. sum of characters in title and description
-    # #  3. use breath first search to extract level of tree in puzzle or during insertion
-    # #  4. number of puzzles before a puzzle in the tree
-    # #  5. number of puzzles after a puzzle in the tree
-    # #  6. time (in minutes - `closed - opened``) spend on solving the puzzle before the current puzzle
-    # #  7. time (in minutes - `closed - opened``) spend on solving the puzzle after the current puzzle
-    # #  8. number of additions in commits before current puzzle
-    # #  9. number of deletions in commits before current puzzle
-    # # 10. vectorized description + title. Using doc2vec with vector size 15 and window size 2.
-    # # Labels (y)
-    # # --- how to get labels ??????
-    # 5. train model with x and y data.
-    # 6. save model weights to s3.
     puzzles = @xml_storage.load
     Thread.new do
       # properly train model here and save weights to s3 for later
@@ -117,7 +99,7 @@ class LinearModel
       unless puzzles.nil?
         samples, labels = extract_features(puzzles['puzzle'])
 
-        solver = Pso::Solver.new(f: clf, center: ZeroVector.zero(labels[0].size), data: samples, true_order: labels)
+        solver = Pso::Solver.new(f: clf, center: ZeroVector.zero(samples[0][0].size), data: samples, true_order: labels)
         _rank, weights, _n_iterations = solver.solve
         @storage.save(weights)
       end
