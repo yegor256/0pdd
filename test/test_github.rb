3b5cd96f9124df72ed0da388a22d36fa2d2d27ba
Natalia Pozhidaeva <Natalia_Pozhidaeva@epam.com>
2024-01-30 09:49:06 -0500

    Update dependencies

--- test/test_github.rb
+++ test/test_github.rb
@@ -29,6 +29,7 @@ require_relative '../objects/clients/github'
 class TestGithub < Test::Unit::TestCase
   def test_configures_everything_right
     github = Github.new.client
-    assert_equal('0pdd', github.user('0pdd')[:login])
+    assert_equal('0pdd', github.user('0pdd')[:login],
+                 "Real user is #{github.user('0pdd')[:login]}")
   end
 end
