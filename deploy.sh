e729bcdd4226b7d30c471d31a68a833ec3b3e692
Yegor Bugayenko <yegor256@gmail.com>
2024-02-13 08:01:40 +0300

    heroku

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cp /code/home/assets/0pdd/config.yml .
 git add config.yml
 git add Gemfile.lock
 git add .gitignore
-git commit -m 'config.yml for dokku'
+git commit -m 'config.yml for heroku'
 trap 'git reset HEAD~1 && rm config.yml && git checkout -- .gitignore' EXIT
-git push dokku master -f
+git push heroku master -f
 
